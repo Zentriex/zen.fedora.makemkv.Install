19a44605b8c95b60d904b85a93c47eb6f3298f1b
zentriex <anonplaguedoctor@gmail.com>
2025-12-19 10:24:49 -0500

    Fixed a url that was missing /old/ since 1.17.7 is a previous version of the application

--- fedora-makemkv-1.17.7.sh
+++ fedora-makemkv-1.17.7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-# Pulls and downloads pre-requisites needed by make-mkv
+# dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Pulls and downloads pre-requisites needed by make-mkv. Only use if needed by FEDORA INSTALL ONLY)
 dnf install -y @c-development @development-tools
 dnf install -y zlib-devel openssl-devel expat-devel ffmpeg ffmpeg-devel qt5-qtbase-devel
 # Installs Necessary Pre-req packages
@@ -10,7 +10,7 @@ mkdir makemkv
 # Make a new directory to save files.
 cd makemkv
 wget https://www.makemkv.com/download/old/makemkv-bin-1.17.7.tar.gz
-wget https://www.makemkv.com/download/makemkv-oss-1.17.7.tar.gz
+wget https://www.makemkv.com/download/old/makemkv-oss-1.17.7.tar.gz
 # Pulls the bin and oss files from the make.mkv website. The version number can be adjusted to whatever version you want to implement.
 gzip -d makemkv-bin-1.17.7.tar.gz | tar xpf makemkv-bin-1.17.7.tar
 gzip -d makemkv-oss-1.17.7.tar.gz | tar xpf makemkv-oss-1.17.7.tar
