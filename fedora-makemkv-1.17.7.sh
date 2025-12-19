28a82bc40f8dc4ff5845f4c464d25092b089c8b1
zentriex <anonplaguedoctor@gmail.com>
2025-12-19 09:51:55 -0500

    Added actual script into file.

--- fedora-makemkv-1.17.7.sh
+++ fedora-makemkv-1.17.7.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y @c-development @development-tools
+dnf install -y zlib-devel openssl-devel expat-devel ffmpeg ffmpeg-devel qt5-qtbase-devel
+cd ~/Downloads
+mkdir makemkv
+cd makemkv
+wget https://www.makemkv.com/download/old/makemkv-bin-1.17.7.tar.gz
+wget https://www.makemkv.com/download/makemkv-oss-1.17.7.tar.gz
+gzip -d makemkv-bin-1.17.7.tar.gz | tar xpf makemkv-bin-1.17.7.tar
+gzip -d makemkv-oss-1.17.7.tar.gz | tar xpf makemkv-oss-1.17.7.tar
+cd makemkv-oss-1.17.7
+./configure
+make
+make install
+cd ../makemkv-bin-1.17.7
+make
+sudo make install
